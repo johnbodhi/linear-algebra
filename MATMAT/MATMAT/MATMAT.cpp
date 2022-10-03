f2ca8c9d5e3529274a2a184017bc7cd48758c345
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2022-10-03 18:26:30 -0500

    Update MATMAT.cpp

--- MATMAT/MATMAT/MATMAT.cpp
+++ MATMAT/MATMAT/MATMAT.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int main() {
 
-	const int M = 5, N = 5, O = 5;
+	const int N = 2, M = 4, O = 4, P = 2;
 
-	int A[N][M], B[N][M], C[N][M];
+	int A[N][M], B[O][P], C[N][P];
 
 	int ii = 0, jj = 0;
-
+	
 	int aa = 0, bb = 0;
 
 	int uu = 0, vv = 0;
@@ -20,20 +20,33 @@ int main() {
 	std::mt19937 mt(rd());
 	std::uniform_real_distribution <double> dist(1.0, 10.0);
 
+	for (int i = 0; i <= N - 1; i++) {
 
-	for (int j = 0; j <= M - 1; j++) {
-
-		for (int i = 0; i <= N - 1; i++) {
-
+		for (int j = 0; j <= M - 1; j++) {
+			
 			A[i][j] = dist(mt);
+		}
+	}
+
+	for (int i = 0; i <= O - 1; i++) {
+
+		for (int j = 0; j <= P - 1; j++) {
+
 			B[i][j] = dist(mt);
+		}
+	}
+
+	for (int i = 0; i <= N - 1; i++) {
+
+		for (int j = 0; j <= P - 1; j++) {
+
 			C[i][j] = 0;
 		}
 	}
 
-	for (int j = 0; j <= M - 1; j++) {
+	for (int i = 0; i <= N - 1; i++) {
 
-		for (int i = 0; i <= N - 1; i++) {
+		for (int j = 0; j <= M - 1; j++) {
 
 			std::cout << setw(5) << A[i][j] << setw(5);
 		}
@@ -42,9 +55,9 @@ int main() {
 	std::cout << std::endl;
 
 
-	for (int j = 0; j <= M - 1; j++) {
+	for (int i = 0; i <= O - 1; i++) {
 
-		for (int i = 0; i <= N - 1; i++) {
+		for (int j = 0; j <= P - 1; j++) {
 
 			std::cout << setw(5) << B[i][j] << setw(5);
 		}
@@ -53,24 +66,24 @@ int main() {
 	std::cout << std::endl;
 
 
-	for (int k = 0; k <= O - 1; k++) {
+	for (int k = 0; k <= N - 1; k++) {
 
-		for (int j = 0; j <= M - 1; j++) {
+		for (int j = 0; j <= P - 1; j++) {
 
-			for (int i = 0; i <= N - 1; i++) {
+			for (int i = 0; i <= M - 1; i++) {
 
 				C[uu][vv] += A[aa][bb] * B[jj][ii];
 
-				aa++; ii++;
+				bb++; jj++;
 			}
-			aa = 0; ii = 0; jj++; uu++;
+			bb = 0; jj = 0; ii++; vv++;
 		}
-		jj = 0; uu = 0; bb++; vv++;
+		ii = 0; vv = 0; aa++; uu++;
 	}
 
-	for (int j = 0; j <= M - 1; j++) {
+	for (int i = 0; i <= P - 1; i++) {
 
-		for (int i = 0; i <= N - 1; i++) {
+		for (int j = 0; j <= N - 1; j++) {
 
 			std::cout << setw(5) << C[i][j] << setw(5);
 		}
